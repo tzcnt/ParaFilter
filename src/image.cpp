92b39b1c5cc222acba6e1c325a0d39e5e3edec5b
omagdy7 <omar.professional8777@gmail.com>
2024-05-04 02:06:46 +0300

    feat: Added a simple abstraction over stbi single header library

---
+++ src/image.cpp
@@ -0,0 +1,48 @@
+#include "include/image.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include "include/stb_image.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "include/stb_image_write.h"
+
+void image_data_deleter(unsigned char *p) { stbi_image_free(p); }
+
+bool Image::save(const char *filename, const char *format) {
+  if (std::string(format) == "png") {
+    return stbi_write_png(filename, this->width, height, channels, data.get(),
+                          this->width * channels);
+  } else if (std::string(format) == "jpg") {
+    return stbi_write_jpg(filename, this->width, height, channels, data.get(),
+                          100); // 100 is the quality parameter
+  } else if (std::string(format) == "bmp") {
+    return stbi_write_bmp(filename, this->width, height, channels, data.get());
+  } else if (std::string(format) == "tga") {
+    return stbi_write_tga(filename, width, height, channels, data.get());
+  }
+  return false; // Unsupported format
+}
+
+bool Image::pad(int borderSize) {
+  int newWidth = width + 2 * borderSize;
+  int newHeight = height + 2 * borderSize;
+  std::unique_ptr<unsigned char, decltype(&image_data_deleter)> newData(
+      new unsigned char[newWidth * newHeight * channels], image_data_deleter);
+
+  // Initialize new image data with padValue
+  memset(newData.get(), 0, newWidth * newHeight * channels);
+
+  // Copy original image data to the center of the new image
+  for (int y = 0; y < height; ++y) {
+    unsigned char *src = data.get() + y * width * channels;
+    unsigned char *dst = newData.get() +
+                         (y + borderSize) * newWidth * channels +
+                         borderSize * channels;
+    memcpy(dst, src, width * channels);
+  }
+
+  // Replace the old data with the new padded data
+  width = newWidth;
+  height = newHeight;
+  data.swap(newData);
+
+  return true;
+}
---
+++ src/include/image.h
@@ -0,0 +1,53 @@
+#pragma once
+#include <memory>
+#include <stdexcept>
+#include <string>
+#include <cstdlib>
+#include "stb_image.h"
+
+void image_data_deleter(unsigned char *p);
+
+struct Image {
+  int width, height, channels;
+  std::unique_ptr<unsigned char, decltype(&image_data_deleter)> data;
+
+  Image(unsigned char *data, int width, int height, int channels)
+      : width(width), height(height), channels(channels),
+        data(data, image_data_deleter) {}
+
+  static Image load(const char *filename) {
+    int width, height, channels;
+    unsigned char *raw_data =
+        stbi_load(filename, &width, &height, &channels, 0);
+    if (!raw_data) {
+      throw std::runtime_error(stbi_failure_reason());
+    }
+    return Image(raw_data, width, height, channels);
+  };
+
+  struct Iterator {
+    unsigned char *ptr;
+    int step;
+
+    Iterator(unsigned char *ptr, int step) : ptr(ptr), step(step) {}
+
+    Iterator &operator++() {
+      ptr += step;
+      return *this;
+    }
+
+    bool operator!=(const Iterator &other) const { return ptr != other.ptr; }
+
+    unsigned char *operator*() { return ptr; }
+  };
+
+  Iterator begin() { return Iterator(data.get(), channels); }
+
+  Iterator end() {
+    return Iterator(data.get() + width * height * channels, channels);
+  }
+
+  bool save(const char *filename, const char *format);
+
+  bool pad(int borderSize);
+};
