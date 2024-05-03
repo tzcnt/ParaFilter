4e6aff4bc61f1743d539330a24fda76f7f2e16ff
omagdy7 <omar.professional8777@gmail.com>
2024-05-03 03:05:08 +0300

    test: Added a simple test for the low pass filter using gtest

---
+++ test/test_main.cpp
@@ -0,0 +1,30 @@
+#include "../src/include/image_processing.h"
+#include <gtest/gtest.h>
+
+TEST(LowPassFilterTest, LowPass3x3) {
+  int width = 5, height = 5,
+      channels = 1; // Slightly larger image to have a meaningful center
+  std::vector<unsigned char> testImage(width * height * channels, 10);
+  testImage[12] = 50;
+
+  std::vector<unsigned char> outputImage(width * height * channels, 0);
+
+  Kernel kernel = kernels[Filter::LowPass3x3];
+  applyKernel(testImage.data(), outputImage.data(), width, height, channels,
+              kernel);
+
+  // Expected output should show the averaging effect, but we ignore the edges
+  int expectedCenterValue = (10 + 10 + 10 + 10 + 50 + 10 + 10 + 10 + 10) / 9;
+  for (int y = 1; y < height - 1; y++) {  // Ignore the top and bottom rows
+    for (int x = 1; x < width - 1; x++) { // Ignore the left and right columns
+      int index = y * width + x;
+      EXPECT_EQ(outputImage[index], expectedCenterValue)
+          << "Pixel index " << index << " did not match expected output.";
+    }
+  }
+}
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
