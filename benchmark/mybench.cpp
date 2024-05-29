914a5e67ed9dc08d1ac3fba87be28e857862f396
tzcnt <trailingzerocount@gmail.com>
2024-05-29 16:13:21 -0700

    setup local testing

--- benchmark/mybench.cpp
+++ benchmark/mybench.cpp
@@ -1,11 +1,11 @@
 #include <benchmark/benchmark.h>
 #include "../src/include/image_processing.h"
 
+static const char *inputFile = "./lena.png";
+static const char *outputFile = "./lena_modified.png";
 static void BM_Sequential(benchmark::State &state) {
   // Perform setup here
   int width, height, channels;
-  const char *inputFile = "./examples/lena.png";
-  const char *outputFile = "./outputs/lena_modified.png";
 
   // Load image
   Image img = Image::load(inputFile);
@@ -19,8 +19,6 @@ static void BM_OpenMP(benchmark::State &state) {
   // Perform setup here
   int width, height, channels;
   auto nthreads = state.range(0);
-  const char *inputFile = "./examples/lena.png";
-  const char *outputFile = "./outputs/lena_modified.png";
 
   // Load image
   Image img = Image::load(inputFile);
@@ -33,6 +31,6 @@ static void BM_OpenMP(benchmark::State &state) {
 
 // Register the function as a benchmark
 BENCHMARK(BM_Sequential)->Unit(benchmark::kMillisecond);
-BENCHMARK(BM_OpenMP)->DenseRange(4, 32, 4)->Unit(benchmark::kMillisecond);
+BENCHMARK(BM_OpenMP)->DenseRange(4, 256, 4)->Unit(benchmark::kMillisecond);
 // Run the benchmark
 BENCHMARK_MAIN();
