ea49d16cf770af54d9d11449e0e27e0bb9845b47
tzcnt <trailingzerocount@gmail.com>
2024-05-29 19:13:17 -0700

    pre-allocate sum vec

--- src/include/image_processing.h
+++ src/include/image_processing.h
@@ -71,10 +71,13 @@ Image applyKernelSeq(Image &img, const Kernel kernel) {
 
   memcpy(output, img.data.get(), img.width * img.height * img.channels);
 
+  std::vector<float> sum(img.channels, 0.0f);
   // Loop over each pixel in the image
   for (int y = kHalf; y < img.height - kHalf; y++) {
     for (int x = kHalf; x < img.width - kHalf; x++) {
-      std::vector<float> sum(img.channels, 0.0f);
+      for (int c = 0; c < img.channels; c++) {
+        sum[c] = 0.0f;
+      }
 
       // Perform the convolution operation
       for (int ky = -kHalf; ky <= kHalf; ky++) {
@@ -121,32 +124,38 @@ Image applyKernelOpenMp(Image &img, const Kernel kernel, int nthreads) {
   omp_set_num_threads(nthreads);
 
   // Loop over each pixel in the image
-#pragma omp parallel for collapse(2)
-  for (int y = kHalf; y < img.height - kHalf; y++) {
-    for (int x = kHalf; x < img.width - kHalf; x++) {
-      std::vector<float> sum(img.channels, 0.0f);
+#pragma omp parallel
+  {
+    std::vector<float> sum(img.channels, 0.0f);
+#pragma omp for collapse(2)
+    for (int y = kHalf; y < img.height - kHalf; y++) {
+      for (int x = kHalf; x < img.width - kHalf; x++) {
+        for (int c = 0; c < img.channels; c++) {
+          sum[c] = 0.0f;
+        }
 
-      // Perform the convolution operation
-      for (int ky = -kHalf; ky <= kHalf; ky++) {
-        for (int kx = -kHalf; kx <= kHalf; kx++) {
+        // Perform the convolution operation
+        for (int ky = -kHalf; ky <= kHalf; ky++) {
+          for (int kx = -kHalf; kx <= kHalf; kx++) {
 
-          int px = (x + kx);
-          int py = (y + ky);
-          const unsigned char *pixel =
-              img.data.get() + (py * img.width + px) * img.channels;
+            int px = (x + kx);
+            int py = (y + ky);
+            const unsigned char *pixel =
+                img.data.get() + (py * img.width + px) * img.channels;
 
-          for (int c = 0; c < img.channels; c++) {
-            sum[c] += pixel[c] * kernel[ky + kHalf][kx + kHalf];
+            for (int c = 0; c < img.channels; c++) {
+              sum[c] += pixel[c] * kernel[ky + kHalf][kx + kHalf];
+            }
           }
         }
+        const unsigned char *pixel =
+            img.data.get() + (y * img.width + x) * img.channels;
+        unsigned char *outPixel = output + (y * img.width + x) * img.channels;
+        for (int c = 0; c < img.channels; c++) {
+          outPixel[c] = static_cast<unsigned char>(clamp((int)sum[c], 0, 255));
+        }
+        outPixel[3] = pixel[3];
       }
-      const unsigned char *pixel =
-          img.data.get() + (y * img.width + x) * img.channels;
-      unsigned char *outPixel = output + (y * img.width + x) * img.channels;
-      for (int c = 0; c < img.channels; c++) {
-        outPixel[c] = static_cast<unsigned char>(clamp((int)sum[c], 0, 255));
-      }
-      outPixel[3] = pixel[3];
     }
   }
   return Image(output, img.width, img.height, img.channels);
